ddcd59560050d31eee5b7f08d4e30e2d8d7aee19
cfowler650 <cfowler650@gmail.com>
2019-11-17 08:28:37 -0600

    Add Select Option Dropdown Food

--- src/components/Calc.jsx
+++ src/components/Calc.jsx
@@ -8,11 +8,26 @@ export default class Calc extends React.Component {
 
                 <div style={{ display: "inline-block", border: "2px solid gray", width: "600px", height: "500px" }}>
                     <div style={{ display: "flex", height: "500px" }}>
+
                         <div style={{ flex: "1", padding: "1em", borderRight: "1px solid gray" }}>
-                            Input
+                            <h2>Input</h2>
+                            <div style={{ margin: "1em" }}>
+                                <p>Choose Food:</p>
+                                <div style={{ margin: "1em", display: "flex" }}>
+                                    <select style={{ width: "100%", height: "40px", fontSize: "20px" }}>
+                                        <option value="volvo">Egg</option>
+                                        <option value="saab">Black Beans</option>
+                                        <option value="mercedes">White Rice</option>
+                                    </select>
+                                </div>
+                            </div>
                         </div>
                         <div style={{ flex: "1", padding: "1em", }}>
-                            Total
+                            <h2>Total</h2>
+
+                            <div>
+
+                            </div>
                         </div>
                     </div>
                 </div>
