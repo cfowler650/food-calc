8a4f2dc7d25dd69c96dc7d750ec411035361ce9f
cfowler650 <cfowler650@gmail.com>
2019-11-17 08:43:47 -0600

    Add calculate button and amount dropdown

--- src/components/Calc.jsx
+++ src/components/Calc.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 
 
 export default class Calc extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+
     render() {
         return (
             <div style={{ textAlign: 'center', margin: "2em" }}>
@@ -10,18 +20,53 @@ export default class Calc extends React.Component {
                     <div style={{ display: "flex", height: "500px" }}>
 
                         <div style={{ flex: "1", padding: "1em", borderRight: "1px solid gray" }}>
+
                             <h2>Input</h2>
                             <div style={{ margin: "1em" }}>
-                                <p>Choose Food:</p>
                                 <div style={{ margin: "1em", display: "flex" }}>
                                     <select style={{ width: "100%", height: "40px", fontSize: "20px" }}>
+                                        <option disabled selected value="volvo">Choose Food</option>
                                         <option value="volvo">Egg</option>
                                         <option value="saab">Black Beans</option>
                                         <option value="mercedes">White Rice</option>
                                     </select>
                                 </div>
                             </div>
+
+
+
+                            <h2>Amount</h2>
+                            <div style={{ margin: "1em" }}>
+                                <div style={{ margin: "1em", display: "flex" }}>
+                                    <select style={{ width: "100%", height: "40px", fontSize: "20px" }}>
+                                        <option disabled selected value="volvo">Quantity</option>
+                                        <option value="volvo">1</option>
+                                        <option value="saab">2</option>
+                                        <option value="mercedes">3</option>
+                                        <option value="mercedes">4</option>
+                                        <option value="mercedes">5</option>
+                                    </select>
+                                </div>
+                            </div>
+
+
+
+                            <button style={{ width: "100px",
+                            height: "40px",
+                            fontSize: "20px",
+                            textTransform: "uppercase",
+                            borderRadius: "2%",
+                            border: "none",
+                            cursor:"pointer",
+                            boxShadow: "2px 2px 2px 1px rgba(0, 0, 0, 0.2)",
+                            backgroundColor: "teal",
+                            color: "white" }}>
+                                Add </button>
+
                         </div>
+
+
+
                         <div style={{ flex: "1", padding: "1em", }}>
                             <h2>Total</h2>
 
@@ -29,6 +74,7 @@ export default class Calc extends React.Component {
 
                             </div>
                         </div>
+
                     </div>
                 </div>
 
