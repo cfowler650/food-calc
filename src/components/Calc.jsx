f22a608b92d1072154fdc6820daa89a9ab65a990
cfowler650 <cfowler650@gmail.com>
2019-11-17 10:16:50 -0600

    state updating correctly

--- src/components/Calc.jsx
+++ src/components/Calc.jsx
@@ -25,19 +25,43 @@ const data = [
     }
 ]
 
+
+
+
 export default class Calc extends React.Component {
 
     constructor(props) {
         super(props);
 
         this.state = {
-            foodSelected: null,
-            amountSelected: 0
+            selectedFood: '',
+            amount: 1
         }
+
+        this.generateTotal = this.generateTotal.bind(this);
     }
+
+   generateTotal(food, amount) {
+        // console.log(food);
+        // console.log(amount);
+        let thisFood = data.find(element => element.name === food )
+        return (
+                <>
+                    <li>Protein: { thisFood.protein * amount} </li>
+                    <li>Fat: { thisFood.fat * amount }</li>
+                    <li>Carbs: { thisFood.carbs * amount }</li>
+                    <li>Calories: { thisFood.calories * amount }</li>
+                </>
+        )
+   }
+
+   handleClick() {
+       console.log(this.state.selectedFood)
+   }
 
 
     render() {
+        const {selectedFood, amount} = this.state;
         return (
             <div style={{ textAlign: 'center', margin: "2em" }}>
 
@@ -49,11 +73,11 @@ export default class Calc extends React.Component {
                             <h2>Input</h2>
                             <div style={{ margin: "1em" }}>
                                 <div style={{ margin: "1em", display: "flex" }}>
-                                    <select style={{ width: "100%", height: "40px", fontSize: "20px" }}>
-                                        <option disabled selected value="volvo">Choose Food</option>
-                                        <option value="volvo">Egg</option>
-                                        <option value="saab">Black Beans</option>
-                                        <option value="mercedes">White Rice</option>
+                                    <select value={selectedFood} onChange={(e) => this.setState({selectedFood: e.target.value})} style={{ width: "100%", height: "40px", fontSize: "20px" }}>
+                                        <option selected value='choose food'>Choose Food</option>
+                                        <option value="egg">Egg</option>
+                                        <option value="black beans">Black Beans</option>
+                                        <option value="white rice">White Rice</option>
                                     </select>
                                 </div>
                             </div>
@@ -85,7 +109,8 @@ export default class Calc extends React.Component {
                             cursor:"pointer",
                             boxShadow: "2px 2px 2px 1px rgba(0, 0, 0, 0.2)",
                             backgroundColor: "teal",
-                            color: "white" }}>
+                            color: "white" }}
+                            onClick={() => {this.handleClick()}}>
                                 Add </button>
 
                         </div>
@@ -99,10 +124,22 @@ export default class Calc extends React.Component {
                                <div style={{display: "flex"}}>
                                    <div style={{flex: "1", padding: "1em 0.5em"}}>
                                         <ul style={{listStyle: "none", textAlign: "left"}}>
-                                            <li>Protein: </li>
-                                            <li>Fat: </li>
-                                            <li>Carbs: </li>
-                                            <li>Calories: </li>
+                                            {
+                                                selectedFood !== '' ?
+                                                  this.generateTotal(selectedFood, amount)
+                                                  :
+                                                 <>
+                                                    {/* <li>Protein</li>
+                                                    <li>Fat</li>
+                                                    <li>Carbs</li>
+                                                    <li>Calories</li> */}
+                                                  </>
+
+
+                                            }
+
+
+
                                         </ul>
 
 
